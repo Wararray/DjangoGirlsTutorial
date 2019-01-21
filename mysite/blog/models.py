5455b53deb831d0b10f9e8f75a7218a1d9abb439
wararray <dias.unb@gmail.com>
2019-01-21 09:01:08 -0200

    Modified templates to display posts from database

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    me = User.objects.get(username='brandon')
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
